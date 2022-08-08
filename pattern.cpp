30b1b5aa085d4eec656183f3a3dfd6c5979a079a
codegear101 <110805375+codegear101@users.noreply.github.com>
2022-08-08 11:01:31 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,20 @@
+// program to draw a pattern of ractangle with stars the no. of rows and columns were given by the user
+//  *****
+//  *****
+//  *****
+//  *****
+//  *****
+
+#include <iostream>
+using namespace std;
+int main(){
+	int rows,columns;
+	cin>>rows>>columns;
+	for(int i=1;i<=rows;i++){
+		for(int j=1;j<=columns;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
